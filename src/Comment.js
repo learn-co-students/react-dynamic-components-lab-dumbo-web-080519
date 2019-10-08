bc8b186c646bb6281aae4c5389ae049c89c3656d
Zermina Ejaz <zerminaejaz1018@gmail.com>
2019-10-08 14:25:29 -0400

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Comment extends Component{
     render(){
         return (
-            <div class="comment">{this.props.commentText}</div>
+            <div className="comment">{this.props.commentText}</div>
         )
     }
 }
